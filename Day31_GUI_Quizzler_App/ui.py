f45b90e63f9aece2829956d7e8daadc21168a02f
imr30 <122212295+imr30@users.noreply.github.com>
2023-07-23 02:49:12 +0530

    Create ui.py

---
+++ Day31_GUI_Quizzler_App/ui.py
@@ -0,0 +1,34 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic")
+        )
+        
+
+
+
+
+
+
