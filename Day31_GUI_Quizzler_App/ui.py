0e872f509fe5c7bdad714d6a221b6fa6c3471035
imr30 <122212295+imr30@users.noreply.github.com>
2023-07-24 11:01:36 +0530

    Update ui.py

--- Day31_GUI_Quizzler_App/ui.py
+++ Day31_GUI_Quizzler_App/ui.py
@@ -25,7 +25,22 @@ class QuizInterface:
             fill=THEME_COLOR,
             font=("Arial", 20, "italic")
         )
-        
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+   
+
 
 
 
