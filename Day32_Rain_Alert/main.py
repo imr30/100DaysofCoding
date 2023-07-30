1d926d38ce7d53e448ca77b12330179d01f5fac5
imr30 <122212295+imr30@users.noreply.github.com>
2023-07-31 00:25:58 +0530

    Create main.py

---
+++ Day32_Rain_Alert/main.py
@@ -0,0 +1,8 @@
+#Note! For the code to work you need to replace all the placeholders with
+#Your own details. e.g. account_sid, lat/lon, from/to phone numbers.
+
+import requests
+import os
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+
