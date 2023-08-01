0f87e618c3f9feb2e7a772e0965bbe9bd1f28d21
imr30 <122212295+imr30@users.noreply.github.com>
2023-08-02 01:17:12 +0530

    Update main.py

--- Day32_Rain_Alert/main.py
+++ Day32_Rain_Alert/main.py
@@ -7,3 +7,4 @@ from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+api_key = os.environ.get("OWM_API_KEY")
